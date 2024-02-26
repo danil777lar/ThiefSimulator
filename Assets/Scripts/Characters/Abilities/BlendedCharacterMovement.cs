d8d3815abcdee3c9381de679bdd06b4ce80d18a4
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-02-26 15:49:49 +0300

    update animations

--- Assets/Scripts/Characters/Abilities/BlendedCharacterMovement.cs
+++ Assets/Scripts/Characters/Abilities/BlendedCharacterMovement.cs
@@ -8,18 +8,25 @@ public class BlendedCharacterMovement : CharacterMovement
 { 
     protected const string _blendAnimationParameterName = "MoveBlend";
     protected int _blendAnimationParameter;
-    
+
+    public float CurrentSpeedPercent { get; private set; }
+
     protected override void InitializeAnimatorParameters()
     {
         base.InitializeAnimatorParameters();
         RegisterAnimatorParameter (_blendAnimationParameterName, AnimatorControllerParameterType.Float, out _blendAnimationParameter);
     }
 
+    public override void ProcessAbility()
+    {
+        base.ProcessAbility();
+        CurrentSpeedPercent = Mathf.Abs(_controller.CurrentMovement.magnitude) / MovementSpeed;
+    }
+
     public override void UpdateAnimator()
     {
         base.UpdateAnimator();
-        MMAnimatorExtensions.UpdateAnimatorFloat(_animator, _blendAnimationParameter, 
-            Mathf.Abs(_controller.CurrentMovement.magnitude) / MovementSpeed,
+        MMAnimatorExtensions.UpdateAnimatorFloat(_animator, _blendAnimationParameter, CurrentSpeedPercent,
             _character._animatorParameters, _character.RunAnimatorSanityChecks);
     }
 }
