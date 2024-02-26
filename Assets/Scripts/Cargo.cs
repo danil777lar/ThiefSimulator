2046acc8b57439b897a6ef2f06aa5b2061d2d173
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-02-27 02:26:40 +0300

    cargo experiments

--- Assets/Scripts/Cargo.cs
+++ Assets/Scripts/Cargo.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Cargo : MonoBehaviour
 {
     [SerializeField] private List<Transform> attachPoints;
+    [SerializeField] private float rotationSpeed;
 
     private Transform _attachTarget;
     private Transform _attachPoint;
@@ -21,7 +22,7 @@ public class Cargo : MonoBehaviour
     public void Attach(Transform attachTarget, Transform attachPoint, float distance)
     {
         _rb.isKinematic = false;
-        
+
         _attachTarget = attachTarget;
         _attachPoint = attachPoint;
         _attachDistance = distance;
@@ -41,6 +42,15 @@ public class Cargo : MonoBehaviour
         _rb.isKinematic = true;
     }
 
+    private void OnDrawGizmos()
+    {
+        if (_rb != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(_rb.worldCenterOfMass, 0.4f);
+        }
+    }
+
     private void FixedUpdate()
     {
         if (_attachTarget != null)
@@ -50,9 +60,20 @@ public class Cargo : MonoBehaviour
 
             float speed = direction.magnitude / Time.fixedDeltaTime;
 
-            _rb.velocity = Vector3.zero;
+            _rb.velocity = _rb.velocity.Y();
             _rb.angularVelocity = Vector3.zero;
             _rb.AddForceAtPosition(direction.normalized * speed, _attachPoint.position, ForceMode.VelocityChange);
+
+            Vector3 balancePoint = (_attachPoint.position - _attachPoint.forward * 10).XZ();
+            Vector3 projNormal = (_attachPoint.position - _attachTarget.position).normalized.XZ();
+            Vector3 projection = _attachPoint.position + Vector3.Project(-_attachPoint.forward * 10, projNormal).XZ();
+            Vector3 balanceDirection = projection - balancePoint;
+            float balanceSpeed = balanceDirection.magnitude * rotationSpeed;
+            _rb.AddForceAtPosition(balanceDirection.normalized * balanceSpeed, balancePoint, ForceMode.VelocityChange);
+            
+            Debug.DrawLine(_attachPoint.position, balancePoint, Color.green);
+            Debug.DrawLine(projection, balancePoint, Color.red);
+            Debug.DrawLine(_attachTarget.position, _attachTarget.position + projNormal * 15f, Color.blue);
         }
     }
 }
