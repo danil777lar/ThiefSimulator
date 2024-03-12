62ead0c588adf2fd94323554a8a68efeacdecf4a
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-03-12 16:26:09 +0300

    lock mini game complete

--- Assets/Scripts/UI/Popups/LockMiniGamePopup.cs
+++ Assets/Scripts/UI/Popups/LockMiniGamePopup.cs
@@ -2,30 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Larje.Core.Services.UI;
+using MoreMountains.Tools;
 using ProjectConstants;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class LockMiniGamePopup : MiniGamePopup
 {
     [SerializeField] private float cylinderRotateSpeed;
     [SerializeField] private float lockerRotateSpeed;
+    [SerializeField] private float unlockerDamageSpeed;
+    [SerializeField] private float unlockRange;
     [Space] 
     [SerializeField] private Transform cylinder;
     [SerializeField] private Transform locker;
-    [Space]
+    [Space] 
     [SerializeField] private Camera miniGameCamera;
     [SerializeField] private RawImage renderImage;
-    [Space]
+    [Space] 
     [SerializeField] private RectTransformEvents rotateCylinderRect;
     [SerializeField] private RectTransformEvents rotateLockerRect;
-    [Space]
+    [Space] 
     [SerializeField] private Button exitButton;
 
+    private bool _controlActive;
     private bool _rotateCylinder;
+    private float _unlockerHealth = 1f;
     private float _cylinderValue = 0f;
     private float _unlockerValue = 0.5f;
+    private float _unlockPoint;
     private MiniGameArgs _args;
     private RenderTexture _texture;
 
@@ -42,22 +49,43 @@ public class LockMiniGamePopup : MiniGamePopup
             rotateCylinderRect.EventPointerDown += (x) => _rotateCylinder = true;
             rotateCylinderRect.EventPointerUp += (x) => _rotateCylinder = false;
 
+            _controlActive = true;
+            _unlockerHealth = 1f;
+            _unlockPoint = Random.Range(0f, 1f);
+
             CreateTexture();
         }
     }
 
     private void Update()
     {
-        _cylinderValue = _rotateCylinder ? 
+        _cylinderValue = _rotateCylinder && _controlActive ? 
             Mathf.Clamp01(_cylinderValue + Time.deltaTime * cylinderRotateSpeed) : 
             Mathf.Clamp01(_cylinderValue - Time.deltaTime * cylinderRotateSpeed);
-        
+        _cylinderValue = Mathf.Clamp(_cylinderValue, 0f, GetDistanceToUnlock());
+
         cylinder.transform.localRotation = Quaternion.Euler(
             Vector3.Lerp(Vector3.zero, Vector3.up * -90f, _cylinderValue));
         
         locker.transform.localRotation = Quaternion.Euler( 
             Vector3.Lerp(Vector3.up * 90f, Vector3.up * -90f, _unlockerValue) - 
             cylinder.transform.localRotation.eulerAngles.Y());
+
+        if (_controlActive)
+        {
+            if (_cylinderValue >= 1f)
+            {
+                Complete();
+            }
+            else if (_cylinderValue >= GetDistanceToUnlock())
+            {
+                _unlockerHealth -= unlockerDamageSpeed * Time.deltaTime;
+                if (_unlockerHealth <= 0f)
+                {
+                    Fail();
+                }
+            }
+        }
     }
 
     private void OnDestroy()
@@ -67,7 +95,10 @@ public class LockMiniGamePopup : MiniGamePopup
 
     private void RotateLocker(PointerEventData data)
     {
-        _unlockerValue = Mathf.Clamp01(_unlockerValue + (data.delta.x / Screen.width) * lockerRotateSpeed);
+        if (_controlActive)
+        {
+            _unlockerValue = Mathf.Clamp01(_unlockerValue + (data.delta.x / Screen.width) * lockerRotateSpeed);
+        }
     }
     
     private void CreateTexture()
@@ -88,12 +119,14 @@ public class LockMiniGamePopup : MiniGamePopup
 
     private void Complete()
     {
+        _controlActive = false;
         _args.OnComplete?.Invoke();
         Close();
     }
     
     private void Fail()
     {
+        _controlActive = false;
         _args.OnFail?.Invoke();
         Close();
     }
@@ -101,6 +134,11 @@ public class LockMiniGamePopup : MiniGamePopup
     private void Exit()
     {
         Close();
+    }
+    
+    private float GetDistanceToUnlock()
+    { 
+        return 1f - Mathf.Clamp01(Mathf.Abs(_unlockerValue - _unlockPoint) - unlockRange); 
     }
 
     public class Args : MiniGamePopup.MiniGameArgs
