ec712a9d267c67be2055b73d774ae00da36e0670
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-03-15 03:41:08 +0300

    enemy patrol update

--- Assets/Scripts/AI/AIActionPatroll.cs
+++ Assets/Scripts/AI/AIActionPatroll.cs
@@ -9,8 +9,9 @@ using UnityEngine.AI;
 
 public class AIActionPatroll : AIAction
 {
+    [SerializeField] private bool directionForward = true;
     [SerializeField] private float distanceOnSpline;
-
+    
     private Transform _target;
     private SplineComputer _spline;
 
@@ -35,10 +36,34 @@ public class AIActionPatroll : AIAction
     {
         if (_spline)
         {
-            Vector3 from = _brain.Owner.transform.position;
-            _target.position = _spline.EvaluatePosition(
-                _spline.Travel(
-                    _spline.Project(from).percent, distanceOnSpline));
+            double percentTravel = _spline.Travel(0f, distanceOnSpline);
+            double targetPercent = _spline.Project(_brain.Owner.transform.position).percent;
+            targetPercent += percentTravel * (directionForward ? 1f : -1f);
+            
+            if (targetPercent > 1f)
+            {
+                if (_spline.isClosed)
+                {
+                    targetPercent -= 1f;
+                }
+                else
+                {
+                    directionForward = !directionForward;
+                }
+            }
+            else if (targetPercent < 0f)
+            {
+                if (_spline.isClosed)
+                {
+                    targetPercent += 1f;
+                }
+                else
+                {
+                    directionForward = !directionForward;
+                }
+            }
+            
+            _target.position = _spline.EvaluatePosition(Mathf.Clamp01((float)targetPercent));
         }
     }
 }
