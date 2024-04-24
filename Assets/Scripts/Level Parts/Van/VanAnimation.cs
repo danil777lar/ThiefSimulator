f6ab27577cc32952ad2609f50eafb2c597da8dee
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-04-24 19:36:47 +0300

    add van physics

--- Assets/Scripts/Level Parts/Van/VanAnimation.cs
+++ Assets/Scripts/Level Parts/Van/VanAnimation.cs
@@ -1,36 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class VanAnimation : MonoBehaviour
 {
     [SerializeField] private Transform model;
+
     [Header("Center Of Mass")] 
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistance = 0.75f;
     [SerializeField] private Vector3 offset = Vector3.up;
-    [SerializeField] private float maxDistance = 1f;
     [Space]
     [SerializeField] private float force = 10f;
-    [SerializeField] private float damping = 10f;
+    [SerializeField] private float drag = 1f;
 
     private Vector3 _velocity;
     private Vector3 _centerOfMass;
-    private VanMovement _movement;
     
     private Vector3 DefaultPosition => model.transform.position + offset;
 
     private void Start()
     {
-        _movement = GetComponent<VanMovement>();
         _centerOfMass = DefaultPosition;
     }
 
     private void FixedUpdate()
     {
-        Vector3 direction = (_centerOfMass - DefaultPosition);
-        _centerOfMass = DefaultPosition + direction.normalized * Mathf.Min(maxDistance, direction.magnitude);
-        
-        _centerOfMass = Vector3.Lerp(_centerOfMass, DefaultPosition, Time.deltaTime * 10f);
+        Vector3 direction = DefaultPosition - _centerOfMass;
+        _centerOfMass = DefaultPosition - (direction.normalized * Mathf.Min(direction.magnitude, maxDistance)); 
+        if (Vector3.Distance(DefaultPosition, _centerOfMass) > minDistance)
+        {
+            _velocity += direction.normalized * (force * Time.fixedDeltaTime);
+        }
+        _velocity *= 1f - Time.fixedDeltaTime * drag;
+        _centerOfMass += _velocity * Time.fixedDeltaTime;
         model.LookAt(_centerOfMass, -transform.forward);
     }
 
