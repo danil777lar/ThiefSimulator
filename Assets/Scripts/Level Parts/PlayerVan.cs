233b379bdb314c342c850e1e1c24b7770a71bed5
Larzhevskiy Daniil <danil777lar@gmail.com>
2024-04-24 17:32:13 +0300

    van movement complete

--- Assets/Scripts/Level Parts/PlayerVan.cs
+++ Assets/Scripts/Level Parts/PlayerVan.cs
@@ -12,16 +12,20 @@ public class PlayerVan : MonoBehaviour
 {
     [Header("Speed")]
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float rotateSpeed = 45f;
-    [SerializeField] private float acceleration = 2f;
-    [Space]
     [SerializeField] private float dragMove = 2f;
     [SerializeField] private float dragStop = 5f;
+    [SerializeField] private float acceleration = 2f;
+    [Space]
+    [SerializeField] private float angularSpeed = 45f;
+    [SerializeField] private float angularDragMove = 1f;
+    [SerializeField] private float angularDragStop = 1f;
 
     [Header("Path")] 
-    [SerializeField] private float minDistance = 10f;
+    [SerializeField] private float startMoveDistance = 15f;
+    [SerializeField] private float stopMoveDistance = 2f;
 
     private bool _move;
+    private float _currentRotationSpeed;
     private Vector3 _currentVelocity;
     private Vector3 _currentDirection;
     private Vector3 _targetPosition;
@@ -45,16 +49,31 @@ public class PlayerVan : MonoBehaviour
     private void FindTargetPosition()
     {
         _targetPosition = _splineComputer.Project(_player.transform.position).position;
-        _move = Vector3.Distance(transform.position, _targetPosition) > minDistance;
+
+        float distance = Vector3.Distance(transform.position, _targetPosition); 
+        if (_move && distance <= stopMoveDistance)
+        {
+            _move = false;
+        }
+
+        if (!_move && distance >= startMoveDistance)
+        {
+            _move = true;
+        }
     }
 
     private void Rotate()
     {
-        float currentRotationSpeed = Mathf.Lerp(0f, rotateSpeed, _currentVelocity.magnitude / speed);
+        float angularSpeedDelta = Mathf.Lerp(0f, angularSpeed, _currentVelocity.magnitude / speed);
         Vector3 direction = _targetPosition - transform.position;
-        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 
-            currentRotationSpeed * Time.fixedDeltaTime);
+        float angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
+        angularSpeedDelta *= (angle < 0f ? -1f : 1f);
+        angularSpeedDelta *= 1f - Mathf.Clamp01((Mathf.Abs(angle) - 180f) / 180f);
+        
+        _currentRotationSpeed += angularSpeedDelta * Time.fixedDeltaTime;
+        _currentRotationSpeed *= 1f - Time.fixedDeltaTime * (_move ? angularDragMove : angularDragStop);
+        
+        transform.rotation *= Quaternion.Euler(Vector3.up * (_currentRotationSpeed * Time.fixedDeltaTime));
 
         if (_move)
         {
