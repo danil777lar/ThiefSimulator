e12c8fc0ea1bd72124bb5f62202a912e2530e005
Daniil Larzhevskii <danil777lar@gmail.com>
2024-05-20 01:23:49 +0400

    win screen add cases sequence

--- Assets/Scripts/UI/Elements/LootCase.cs
+++ Assets/Scripts/UI/Elements/LootCase.cs
@@ -1,23 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LootCase : MonoBehaviour
 {
+    [SerializeField] private MMF_Player showFeedback;
+    [SerializeField] private MMF_Player openFeedback;
+    
     private Camera caseCamera;
     private RenderTexture texture;
+
+    public event Action EventShown;
+    public event Action EventOpened;
     
-    public RenderTexture GetTexture(int width, int height)
+    public RenderTexture Show(int width, int height)
     {
         texture ??= new RenderTexture(width, height, 24);
         caseCamera.targetTexture = texture;
+        
+        showFeedback?.PlayFeedbacks();
+        
         return texture;
+    }
+
+    public void Open()
+    {
+        openFeedback?.PlayFeedbacks();
     }
 
     private void Awake()
     {
         caseCamera = GetComponentInChildren<Camera>();
+        
+        showFeedback.Events.OnComplete.AddListener(() => EventShown?.Invoke());
+        openFeedback.Events.OnComplete.AddListener(() => EventOpened?.Invoke());
     }
 
     private void OnDestroy()
--- Assets/Scripts/UI/Screens/WinScreen.cs
+++ Assets/Scripts/UI/Screens/WinScreen.cs
@@ -4,27 +4,110 @@ using System.Collections.Generic;
 using Larje.Core.Services;
 using Larje.Core.Services.UI;
 using ProjectConstants;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WinScreen : UIScreen
 {
+    [Header("Buttons")]
+    [SerializeField] private Button openButton;
     [SerializeField] private Button skipButton;
+
+    [Header("Reward")] 
+    [SerializeField] private GameObject bestRewardRoot;
+    [SerializeField] private GameObject rewardItem;
+    [SerializeField] private TextMeshProUGUI rewardCoins;
+    
+    [Header("UI")]
     [SerializeField] private RawImage caseImage;
+
+    [Space]
+    [SerializeField] private float nextStepDelay;
     [SerializeField] private List<LootStep> lootSteps;
 
     [InjectService] private ILevelManagerService _levelService;
     [InjectService] private UIService _uiService;
 
+    private bool _bestRewardGiven;
+    private int _currentStepIndex;
+    
     protected override void OnBeforeOpen(UIObject.Args args)
     {
         ServiceLocator.Instance.InjectServicesInComponent(this);
+        
+        openButton.onClick.AddListener(OnOpenButtonClicked);
         skipButton.onClick.AddListener(OnSkipButtonClicked);
+        
+        bestRewardRoot.SetActive(false);
+        openButton.gameObject.SetActive(false);
+        skipButton.gameObject.SetActive(false);
+        
+        lootSteps.ForEach(x =>
+        {
+            x.Case.EventShown += OnCaseShown;
+            x.Case.EventOpened += OnCaseOpened;
+            x.Case.gameObject.SetActive(false);
+        });
         
         ShowCase();
     }
+    
+    private void ShowCase()
+    {
+        LootStep lootStep = lootSteps[_currentStepIndex];
+        lootStep.Case.gameObject.SetActive(true);
+        
+        RectTransform imageTransform = caseImage.rectTransform;
+        caseImage.texture = lootStep.Case.Show((int)imageTransform.rect.width, (int)imageTransform.rect.height);
+    }
+    
+    private void OnCaseShown()
+    {
+        openButton.gameObject.SetActive(true);
+        bestRewardRoot.SetActive(true);
+    }
+
+    private void OnOpenButtonClicked()
+    {
+        lootSteps[_currentStepIndex].Case.Open();
+        
+        bestRewardRoot.SetActive(false);
+        openButton.gameObject.SetActive(false);
+    }
+    
+    private void OnCaseOpened()
+    {
+        rewardCoins.gameObject.SetActive(true);
+        StartCoroutine(NextStepDelayCoroutine());
+    }
+
+    private void NextStep()
+    {
+        if (_currentStepIndex >= lootSteps.Count - 1 || _bestRewardGiven)
+        {
+            CloseScreen();
+        }
+        else
+        {
+            lootSteps[_currentStepIndex].Case.gameObject.SetActive(false);
+            rewardCoins.gameObject.SetActive(false);
+            rewardItem.gameObject.SetActive(false);
+            bestRewardRoot.SetActive(false);
+            openButton.gameObject.SetActive(false);
+            skipButton.gameObject.SetActive(false);
+            
+            _currentStepIndex++;
+            ShowCase();
+        }
+    }
 
     private void OnSkipButtonClicked()
+    {
+        CloseScreen();
+    }
+
+    private void CloseScreen()
     {
         _levelService.IncrementLevelId();
         _levelService.SpawnCurrentLevel();
@@ -32,10 +115,10 @@ public class WinScreen : UIScreen
             .OpenScreen(new LoadingScreen.Args(false,null));
     }
 
-    private void ShowCase()
+    private IEnumerator NextStepDelayCoroutine()
     {
-        RectTransform imageTransform = caseImage.rectTransform;
-        caseImage.texture = lootSteps[0].Case.GetTexture((int)imageTransform.rect.width, (int)imageTransform.rect.height);
+        yield return new WaitForSeconds(nextStepDelay);
+        NextStep();
     }
 
     public class Args : UIScreen.Args
