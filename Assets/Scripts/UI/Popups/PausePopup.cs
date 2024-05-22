aaefc7f022cfec441812c83ce256c6d75ee9648a
Daniil Larzhevskii <danil777lar@gmail.com>
2024-05-22 23:43:38 +0400

    pause popup buttons complete

--- Assets/Scripts/UI/Popups/PausePopup.cs
+++ Assets/Scripts/UI/Popups/PausePopup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Larje.Core.Services;
 using Larje.Core.Services.UI;
 using ProjectConstants;
 using UnityEngine;
@@ -10,9 +11,14 @@ public class PausePopup : UIPopup
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button restartButton;
     [SerializeField] private Button mainMenuButton;
+
+    [InjectService] private UIService _uiService;
+    [InjectService] private ILevelManagerService _levelManagerService;
     
     protected override void OnBeforeOpen(UIObject.Args args)
     {
+        ServiceLocator.Instance.InjectServicesInComponent(this);
+        
         resumeButton.onClick.AddListener(OnResumeButtonClicked);
         restartButton.onClick.AddListener(OnRestartButtonClicked);
         mainMenuButton.onClick.AddListener(OnMainMenuButtonClicked);
@@ -26,11 +32,22 @@ public class PausePopup : UIPopup
     private void OnRestartButtonClicked()
     {
         Close();
+        
+        _levelManagerService.SpawnCurrentLevel();
+        _uiService.GetProcessor<UIScreenProcessor>().OpenScreen(new LoadingScreen.Args(true,
+            () =>
+            {
+                //_uiService.GetProcessor<UIScreenProcessor>().OpenScreen(new LoadingScreen.Args();
+                _levelManagerService.TryStartCurrentLevel(new LevelProcessor.StartData(LevelStartType.Start));
+            }));
     }
 
     private void OnMainMenuButtonClicked()
     {
         Close();
+        
+        _levelManagerService.SpawnCurrentLevel();
+        _uiService.GetProcessor<UIScreenProcessor>().OpenScreen(new LoadingScreen.Args(true, null));
     }
 
     public new class Args : UIPopup.Args
