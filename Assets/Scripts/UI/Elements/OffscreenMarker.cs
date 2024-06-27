bab9495daca559a62e0a55a5c2a6d61a2a6beca4
Daniil Larzhevskii <danil777lar@gmail.com>
2024-06-28 04:06:19 +0600

    update exit and sell point markers

--- Assets/Scripts/UI/Elements/OffscreenMarker.cs
+++ Assets/Scripts/UI/Elements/OffscreenMarker.cs
@@ -6,8 +6,11 @@ using UnityEngine.Serialization;
 
 public class OffscreenMarker : MonoBehaviour
 {
+    [SerializeField] private float distance;
     [SerializeField] private RectTransform iconRoot;
-    
+    [SerializeField] private RectTransform arrow;
+
+    private CanvasGroup _canvasGroup;
     private Transform _target;
     private Func<bool> _isActive;
 
@@ -16,21 +19,30 @@ public class OffscreenMarker : MonoBehaviour
         _target = target;
         _isActive = isActive;
         
+        _canvasGroup = GetComponent<CanvasGroup>();
+        
         return this;
     }
     
     private void Update()
     {
         if (_target == null) return;
-        
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(_target.position);
-        bool isOnScreen = screenPos.x > 0f && screenPos.x < Screen.width && screenPos.y > 0f && screenPos.y < Screen.height;
-        iconRoot.gameObject.SetActive(!isOnScreen && _isActive.Invoke());
 
-        Vector2 size = iconRoot.sizeDelta; 
-        Vector3 iconRootPosition = screenPos;
-        iconRootPosition.x = Mathf.Clamp(iconRootPosition.x, size.x * 0.5f, Screen.width - size.x * 0.5f);
-        iconRootPosition.y = Mathf.Clamp(iconRootPosition.y, size.y * 0.5f, Screen.height - size.y * 0.5f);
-        iconRoot.position = iconRootPosition;
+        Vector3 centerPosition = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(_target.position);
+        
+        bool isOnScreen = screenPos.x > 0f && screenPos.x < Screen.width && screenPos.y > 0f && screenPos.y < Screen.height;
+        bool isActive = !isOnScreen && _isActive.Invoke();
+        _canvasGroup.alpha = Mathf.Lerp(_canvasGroup.alpha, isActive ? 1f : 0f, Time.deltaTime * 10f);
+        
+        Vector3 direction = (screenPos - centerPosition).normalized;
+        iconRoot.position = centerPosition + direction * distance;
+
+        if (arrow != null)
+        {
+            float arrowAngle = Vector2.SignedAngle(Vector2.right, screenPos - iconRoot.position);
+            Quaternion arrowRotation = Quaternion.Euler(Vector3.forward * arrowAngle);
+            arrow.localRotation = arrowRotation;
+        }
     }
 }
