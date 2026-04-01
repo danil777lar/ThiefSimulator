a7843782db265db272830b6103ed06d09b5ec508
Danil Kolbasenko <danil777lar@gmail.com>
2026-04-01 00:34:01 -0300

    add ui sounds

---
+++ Assets/Plugins/ProjectConstants/SoundType.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Larje.Core.Services
+{
+//SCRIPT IS GENERATED AUTOMATICALLY
+//TO CHANGE THE CONTENT YOU SHOULD USE THE CONFIG
+	public enum SoundType
+	{
+		UI_Click = 0,
+		UI_Popup = 1,
+		Game_Van_Doors = 2,
+		Game_Van_Idle = 3,
+		Game_Van_Move = 4,
+	}
+
+}
