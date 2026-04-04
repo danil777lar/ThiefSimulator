e54753f37b6f7ff3ff1b5b3d04d1a98f669c50ec
Danil Kolbasenko <danil777lar@gmail.com>
2026-04-04 16:53:14 -0300

    add shake effect on player damage

--- Assets/Scripts/Character/CharacterDamageEffect.cs
+++ Assets/Scripts/Character/CharacterDamageEffect.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using Larje.Character;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CharacterDamageEffect : MonoBehaviour
@@ -11,31 +12,40 @@ public class CharacterDamageEffect : MonoBehaviour
 
     [Header("Shake")]
     [SerializeField] private bool useCameraShake = false;
+    [SerializeField] private float shakeIntensity = 1f;
 
     [Header("Sound")]
     [SerializeField] private SoundSettings damageSound;
 
     private Character _character;
     private MaterialSetter _materialSetter;
+    private CinemachineImpulseSource _impulseSource;
 
     private void Start()
     {
         _character = GetComponentInParent<Character>();
         _materialSetter = _character.GetComponentInChildren<MaterialSetter>();
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
 
         _character.Health.EventDamage += OnDamageTaken;
     }
 
     private void OnDamageTaken(DamageData damage)
     {
-        if (changeMaterial)
+        if (changeMaterial && _materialSetter != null)
         {
             _materialSetter.AddMaterialOverride(this, damageMaterial);
             DOVirtual.DelayedCall(materialChangeDuration, () => _materialSetter.RemoveMaterialOverride(this));
         }
 
-        if (useCameraShake)
+        if (useCameraShake && _impulseSource != null)
         {
+            Vector2 impulse = Vector2.zero;
+            impulse.x = Random.Range(-1f, 1f);
+            impulse.y = Random.Range(-1f, 1f);
+            impulse *= shakeIntensity;
+
+            _impulseSource.GenerateImpulse(impulse);
         }
 
         damageSound.Play();
